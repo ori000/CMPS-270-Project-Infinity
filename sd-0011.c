2c18e9c53b193cc464f6f82b79f13b42d6c9894a
shaaban01 <91268135+shaaban01@users.noreply.github.com>
2022-09-11 16:47:29 +0300

    connectFour.c
    - we can say this is the new start of our project
    - changing the structure of the code and solving the bugs
    - adding functions to help the process

--- sd-0011.c
+++ sd-0011.c
@@ -1,260 +1,166 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-#include <conio.h>
+// #include <conio.h>
 
-// struct variables
+// for next version:
+// working on time and tie case -> osama
+// finishing the diagonal check -> Ayla
+// work on pre-game: the toss and players names -> iyad
+
+
+// int hour=0;minute=0;second=0;flag=0;
+
+#define ROWS    6
+#define COLS    7
+
+int matrix[ROWS][COLS];
+
+char border[] = " ----------------------------";
+char guide[] = "  0   1   2   3   4   5   6  ";
+int token = 1;
+int selected = 0;
+
+void add_token();
+
+// void delay(ms)
 // {
-//     int v00;
-//     int v01;
-//     int v02;
-//     int v03;
-//     int v04;
-//     int v05;
-//     int v06;
-//     int v10;
-//     int v11;
-//     int v12;
-//     int v13;
-//     int v14;
-//     int v15;
-//     int v16;
-//     int v20;
-//     int v21;
-//     int v22;
-//     int v23;
-//     int v24;
-//     int v25;
-//     int v26;
-//     int v30;
-//     int v31;
-//     int v32;
-//     int v33;
-//     int v34;
-//     int v35;
-//     int v36;
-//     int v40;
-//     int v41;
-//     int v42;
-//     int v43;
-//     int v44;
-//     int v45;
-//     int v46;
-//     int v50;
-//     int v51;
-//     int v52;
-//     int v53;
-//     int v54;
-//     int v55;
-//     int v56;
-// };
+//     clock_t timeDelay = ms + clock();
+//     while(timeDelay > clock());
+// }
 
-int hour=0;minute=0;second=0;flag=0;
-int input[2];
-int matrix[6][7];
+// int counter()
+// {
+//     while(!kbhit() && flag == 0)
+//     {
+//         if(minute > 59)
+//         {
+//             minute = 0;
+//             ++hour;
+//         }
+//         if(second > 59)
+//         {
+//             second = 0;
+//             ++minute;
+//         }
+//         ++second;
+//     }
+// }
 
-void delay(ms)
-{
-    clock_t timeDelay = ms + clock();
-    while(timeDelay > clock());
-}
 
-void printMatrix(int matrix[6][7])
-{
-    for(int i = 0; i <= 5; i++)
-    {
-        printf("|");
-        for(int j = 0; j <= 6; j++)
-        {
-            printf("%d",matrix[i][j]);
-            printf("|");
+//fill the array with empty values
+void createMatrix(){
+    for(int i=0;i<ROWS;i++){
+        for(int j=0;j<COLS;j++){
+            matrix[i][j] = 0;
         }
     }
 }
+//print the matrix with the current results
+void display(){
+    printf("%s\n", border);
+    for(int i=0;i<ROWS;i++){
+        for(int j=0;j<COLS;j++){
+            printf("| %d ", matrix[i][j]);
+        }
+        printf("|\n");
+    }
+    printf("%s\n", border);
+    printf("%s\n", guide);
 
-int counter()
-{
-    while(!kbhit() && flag == 0)
-    {
-        if(minute > 59)
-        {
-            minute = 0;
-            ++hour;
-        }
-        if(second > 59)
-        {
-            second = 0;
-            ++minute;
-        }
-        ++second;
+}
+
+// selecting which column
+void selecting(){
+
+    printf("\nChoose column: ");
+    scanf(" %d", &selected);
+
+    if(( selected <= 6 && selected >= 0)  && matrix[0][selected] == 0 ){
+        add_token();
+        token = (token == 1 )? 2 : 1; // flipping the token after adding it
+    }
+    else{
+        printf("Invalid selection. Please select a column between 0 and 6");
+        selecting();
+        
     }
 }
-//Check Horizontal
-void checkH(int r, int c)
-{
-    for(int i = 0; i <= 5; i++)
-    {
-        for(int j = 0; j <= 3; j++)
-        {
-            if(matrix[i][j] == 1 && matrix[i][j+1] == 1 && matrix[i][j+2] == 1 && matrix[i][j+3] == 1)
-            {
-                printf("Player 1 won!");
-                checkWinPlayer1 = 1;
-                break;
-            }
-            else if(matrix[i][j] == 2 && matrix[i][j+1] == 2 && matrix[i][j+2] == 2 && matrix[i][j+3] == 2)
-            {
-                printf("Player 2 won!");
-                checkWinPlayer2 = 1;
-                break;
-            }
-        }
-    }
-}
-//Check Vertical
-void checkV(int r, int c)
-{
-    for(int j = 0; j <= 6; j++)
-    {
-        for(int i = 0; i <= 2; i++)
-        {
-            if(matrix[i][j] == 1 && matrix[i+1][j] == 1 && matrix[i+2][j] == 1 && matrix[i+3][j] == 1)
-            {
-                printf("Player 1 won!");
-                checkWinPlayer1 = 1;
-                break;
-            }
-            else if(matrix[i][j] == 2 && matrix[i+1][j] == 2 && matrix[i+2][j] == 2 && matrix[i+3][j] == 2)
-            {    
-                printf("Player 2 won!");
-                checkWinPlayer2 = 1;
-                break;
-            }
+//adding the token to the last available row
+void add_token(){
+    int curRow;
+    for(curRow=ROWS-1; curRow>=0; curRow--){
+        if(matrix[curRow][selected] == 0){
+            matrix[curRow][selected] = token;
+            break;
         }
     }
 }
-
-/*
-    ASC
-
-    i=3j=0, i=2j=1, i=1j=2, i=0j=3
-    i=4j=0, i=3j=1, i=2j=2, i=1j=3, i=0j=4
-    i=5j=0, i=4j=1, i=3j=2, i=2j=3, i=1j=4, i=0j=5
-    i=5j=1, i=4j=2, i=3j=3, i=2j=4, i=1j=5, i=0j=6
-    i=5j=2, i=4j=3, i=3j=4, i=2j=5, i=1j=6
-    i=5j=3, i=4j=2, i=3j=1, i=2j=0
-
-    DESC
-
-    i=0j=3, i=1j=4, i=2j=5, i=3j=6
-    i=0j=2, i=1j=3, i=2j=4, i=3j=5, i=4j=6
-    i=0j=1, i=1j=2, i=2j=3, i=3j=4, i=4j=5, i=5j=6
-    i=0j=0, i=1j=1, i=2j=2, i=3j=3, i=4j=4, i=5j=5, i=6j=6
-    i=1j=0, i=2j=1, i=3j=2, i=4j=3, i=5j=4
-    i=2j=0, i=3j=1, i=4j=2,i=5,j=3
-*/
-
-//Check Oblique (ascending & descending) for Winning
-void checkO_Asc(int r, int c)
-{
-    for(int i = 5; i >= 0; i--)
+int CheckHorizontal(int token){
+    int counter;
+    
+    for (int i = 0; i < ROWS; ++i)
     {
-        for(int j = 0; j <= 6; j++)
+        for (int j = 0; j < 4; ++j) // 4 is the number of ways of connecting four tokens in one row
         {
-            if(matrix[i][j] == 1 && matrix[i-1][j+1] == 1 && matrix[i-2][j+2] == 1 && matrix[i-3][j+3] == 1)
+            counter = 0;
+            for (int k = 0; k < 4; ++k)// 4 in a row
             {
-                printf("Player 1 won!");
-                checkWinPlayer1 = 1;
-                break;
+                if (matrix[i][j + k] == token) counter++;
             }
-            else if(matrix[i][j] == 2 && matrix[i-1][j+1] == 2 && matrix[i-2][j+2] == 2 && matrix[i-3][j+3] == 2)
+            if (counter == 4) return 1;
+        }
+
+    }
+}
+
+int CheckVertical(int token){
+    int counter;
+    
+    for (int i = 0; i < COLS; ++i)
+    {
+        for (int j = 0; j < 3; ++j) // 4 is the number of ways of connecting four tokens in one cloumn
+        {
+            counter = 0;
+            for (int k = 0; k < 4; ++k)// 4 in a row
             {
-                printf("Player 2 won!");
-                checkWinPlayer2 = 1;
-                break;
+                if (matrix[i+ k][j] == token) counter++;
             }
+            if (counter == 4) return 1;
         }
     }
 }
-void checkO_Desc(int r, int c)
-{
-    for(int i = 5; i >= 0; i--)
-    {
-        for(int j = 6; j >= 0; j--)
-        {
-            if(matrix[i][j] == 1 && matrix[i-1][j-1] == 1 && matrix[i-2][j-2] == 1 && matrix[i-3][j-3] == 1)
-            {
-                printf("Player 1 won!");
-                checkWinPlayer1 = 1;
-                break;
-            }
-            else if(matrix[i][j] == 2 && matrix[i-1][j-1] == 2 && matrix[i-2][j-2] == 2 && matrix[i-3][j-3] == 2)
-            {
-                printf("Player 2 won!");
-                checkWinPlayer2 = 1;
-                break;
-            }
-        }
-    }
+// CheckDiagonals(token){
+
+// }
+
+int check(int token){
+    return CheckHorizontal(token) || CheckVertical(token);
+    // CheckDiagonals(token);
 }
 
 int main()
 {
-    for(int i = 0; i <= 5; i++)
-    {
-        for (int j = 0; j <=6; j++)
-        {
-            matrix[i][j] = 0;
-        }
-    }
+    createMatrix();
+    display();
+        while(1){
 
-    while(minute < 20 && checkWinPlayer1 == 0 && checkWinPlayer2 == 0)
-    counter();
-    {
-    printf("Player 1, where do you want to place your chip? (Insert row,column)" );
-    scanf("%d", input);
-    for(int i = 0; i <= 5; i++)
-    {
-        if(i == input[0])
-        for(int j = 0; j <= 6; j++)
-        {
-            if(j == input[1])
+            printf("Player %c, your turn!\n", token);
+            selecting();
+            printf("\n\n");
+            display();
+            if (check(1))
             {
-                matrix[i][j] = 1;
-                printMatrix(matrix[i][j]);
-                checkH(i,j);
-                checkV(i,j);
-                checkO_Asc(i,j);
-                checkO_Desc(i,j);
-                if(checkWinPlayer1 == 1)
-                break;
-                else if(checkWinPlayer2 == 1)
-                break;
+                printf("\n\nPlayer 1 wins!\n\n");
+                break;  
             }
-        }
-    }
-    printf("Player 2, where do you want to place your chip? (Insert row,column)" );
-    scanf("%d", input);
-    for(int i = 0; i <= 5; i++)
-    {
-        if(i == input[0])
-        for(int j = 0; j <= 6; j++)
-        {
-            if(j == input[1])
+            if (check(2))
             {
-                matrix[i][j] = 2;
-                printMatrix(matrix[i][j]);
-                checkH(i,j);
-                checkV(i,j);
-                checkO_Asc(i,j);
-                checkO_Desc(i,j);
-                if(checkWinPlayer1 == 1)
-                break;
-                else if(checkWinPlayer2 == 1)
-                break;
-            }
+                printf("\n\nPlayer 2 wins!\n\n");
+                break;  
+            }   
         }
-    }
-    }
     return 0;
 }
+        
+
